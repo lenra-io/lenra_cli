8738443f18c8e5cbc4b18df9812c4b9b338e0b30
Thomas DA ROCHA <thomas.darocha@lenra.io>
2022-08-13 02:10:08 +0200

    Init project

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use clap::Parser;
+use cli::{Cli, CliSubcommand};
+use env_logger;
+
+mod build;
+mod cli;
+mod config;
+mod dev;
+mod new;
+mod start;
+
+fn main() {
+    env_logger::init();
+    let args = Cli::parse();
+    // TODO: manage config file
+    args.command.run();
+}
